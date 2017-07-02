4bab81b180bac56a3822c74d0f8d89e57dc9068c
권용근 <kingbbode@gmail.com>
2017-07-02 21:38:15 +0900

    setting project default dependencies

---
+++ src/main/java/com/kingbbode/JooqTutorialApplication.java
@@ -0,0 +1,12 @@
+package com.kingbbode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqTutorialApplication.class, args);
+	}
+}
